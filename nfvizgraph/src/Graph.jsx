a3220168f56ce1a47314aa83c17a585f5932b368
illymakes <illymakes23@gmail.com>
2024-04-26 00:33:33 -0400

    fixed graph centering and styling

--- nfvizgraph/src/Graph.jsx
+++ nfvizgraph/src/Graph.jsx
@@ -9,14 +9,18 @@ function Graph() {
     const svgRef = useRef(null);
     const containerRef = useRef(null);
     const initialized = useRef(false);
+    const zoomRef = useRef(d3.zoom().scaleExtent([0.1, 8]));
 
     useEffect(() => {
         const svg = d3.select(svgRef.current)
+            .attr('width', '100vw')
+            .attr('height', '100%')
             .style('background-color', "#121212");
 
         if (!initialized.current) {
             initialized.current = true;
             initializeGraph(svg);
+            setTimeout(() => setInitialZoom(svg), 100);
         }
 
         const zoomBehavior = d3.zoom()
@@ -29,9 +33,9 @@ function Graph() {
 
         const resizeObserver = new ResizeObserver(entries => {
             if (entries.length === 0 || entries[0].target !== containerRef.current) return;
-            const { width, height } = entries[0].contentRect;
-            svg.attr("width", width)
-                .attr("height", height);
+            svg.attr("width", '100vw')
+                .attr("height", '100%');
+            setInitialZoom(svg); // Reapply the initial zoom when resized
         });
 
         resizeObserver.observe(containerRef.current);
@@ -41,21 +45,36 @@ function Graph() {
         };
     }, []);
 
+    function setInitialZoom(svg) {
+        const svgRect = svg.node().getBoundingClientRect();
+        const width = svgRect.width;
+        const height = svgRect.height;
+
+        const initialTransform = d3.zoomIdentity
+            .translate(width / 2, height / 2)
+            .scale(0.9)
+            .translate(-width / 2, -height / 2);
+
+        svg.transition().duration(750).call(zoomRef.current.transform, initialTransform);
+    }
+
     function initializeGraph(svg) {
-        const width = svg.style("width");
-        const height = svg.style("height");
+        const width = parseInt(svg.style("width"));
+        const height = parseInt(svg.style("height"));
 
         svg.attr("width", width)
             .attr("height", height);
-            const graphContainer = svg.append('g');
+
+        const graphContainer = svg.append('g');
+
 
         d3.csv('./games.csv').then(data => {
-            const nodes = data.map(d => ({ 
-                id: d.Name, 
+            const nodes = data.map(d => ({
+                id: d.Name,
                 group: d.Developer,
                 console: d.Console,
                 year: d.Year,
-                publisher: d.Publisher 
+                publisher: d.Publisher
             }));
             const links = [];
             const developerMap = {};
@@ -78,17 +97,25 @@ function Graph() {
                 });
             }
 
+            const initialLinkDistance = 150;
+            const initialChargeStrength = -50;
+
             const simulation = d3.forceSimulation(nodes)
-                .force("link", d3.forceLink(links).id(d => d.id))
-                .force("charge", d3.forceManyBody())
-                .force("center", d3.forceCenter(svg.attr("width") / 2, svg.attr("height") / 2));
+                .force('link', d3.forceLink(links).id(d => d.id).distance(initialLinkDistance))
+                .force('charge', d3.forceManyBody().strength(initialChargeStrength).distanceMax(275))
+                .force('center', d3.forceCenter(width / 2, height / 2))
+                .force('collide', d3.forceCollide(5))
+                .force('x', d3.forceX().strength(0.1))
+                .force('y', d3.forceY().strength(0.1))
+                .force('label', d3.forceManyBody().strength(-1000));
 
             const link = graphContainer.append("g")
                 .selectAll("line")
                 .data(links)
                 .enter().append("line")
-                .attr("stroke", "#999")
-                .attr("stroke-opacity", 0.6);
+                .attr("stroke", "#ff00f3")
+                .attr("stroke-opacity", 0.2)
+                .attr('stroke-width', '1.5');
 
             const node = graphContainer.append("g")
                 .selectAll("circle")
@@ -96,9 +123,24 @@ function Graph() {
                 .enter().append("circle")
                 .attr("r", 5)
                 .attr("fill", d => colorByConsole(d.console))
-                .attr('stroke', '#999')
+                .attr('stroke', '#ff00f3')
                 .attr('stroke-width', '1.5')
                 .call(drag(simulation));
+
+            const labels = graphContainer.append("g")
+                .selectAll("text")
+                .data(nodes, d => d.id)
+                .enter().append("text")
+                .text(d => d.id)
+                .attr("x", d => d.x)
+                .attr("y", d => d.y)
+                .style('fill', '#fff')
+                .style('font-size', '16px')
+                .style("font-family", "Roboto Mono, Arial, sans-serif")
+                .attr("text-anchor", "right")
+                .attr("dy", "-0.5em");
+
+            node.call(drag(simulation));
 
             simulation.on("tick", () => {
                 link.attr("x1", d => d.source.x)
@@ -108,15 +150,18 @@ function Graph() {
 
                 node.attr("cx", d => d.x)
                     .attr("cy", d => d.y);
+
+                labels.attr('x', d => d.x + 8)
+                    .attr('y', d => d.y) + 3;
             });
         });
     }
 
     function colorByConsole(consoleName) {
         const colorMap = {
-            'Switch': '#FF6347',   // red
+            'Switch': '#FF7AF3',   // pink
             'N64': '#50C878',    // green
-            'GameCube': '#DDA0DD',  // purple
+            'GameCube': '#36368a',  // purple
             'Wii': '#5BE3FF',    // light blue
             'WiiU': '#005CA6',   // blue
             'SNES': '#cccccc'  // gray
--- nfvizgraph/src/UploadModal.jsx
+++ nfvizgraph/src/UploadModal.jsx
@@ -43,7 +43,7 @@ const UploadModal = ({ isVisible, onClose, onFileUpload }) => {
         <h5>Upload your CSV file:</h5>
         <div className="dropzone" id="drag-drop-area" onDragOver={handleDragOver} onDrop={handleDrop}>
           Drag and drop your CSV here or click&nbsp;
-          <span id="browse-button" style={{curosr: 'pointer'}}>browse</span>
+          <span id="browse-button" style={{cursor: 'pointer'}}>browse</span>
         </div>
         <input type="file" accept=".csv" style={{ display: 'none' }} onChange={handleFileChange} ref={fileInputRef} />
         <h5>Or paste in your CSV data:</h5>
